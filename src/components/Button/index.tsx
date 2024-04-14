cde9b240df765358f783424abc2175884d228068
raposo5000 <felipesantosdemenezes1998@gmail.com>
2024-04-14 18:55:05 -0300

    feat: async Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { StyledButton } from "./styles";
+import CircularProgress from "../CircularProgress";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  fullWidth?: boolean
+  fullWidth?: boolean;
+  onClick: () => Promise<void>;
 }
 
-const Button = ({ fullWidth, ...props }: ButtonProps) => {
-  return <StyledButton {...props} fullWidth={fullWidth} />;
+const Button = ({
+  onClick,
+  fullWidth,
+  disabled,
+  children,
+  ...props
+}: ButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    if (disabled) {
+      return; // Se o botão estiver desabilitado, não faz nada
+    }
+
+    setIsLoading(true);
+    try {
+      await onClick();
+    } catch (error) {
+      // Trate o erro, se necessário
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <StyledButton
+      {...props}
+      fullWidth={fullWidth}
+      disabled={isLoading || disabled}
+      onClick={handleClick}
+    >
+      {isLoading ? (
+        <CircularProgress color="white" />
+      ) : (
+        children
+      )}
+    </StyledButton>
+  );
 };
 
 export default Button;
