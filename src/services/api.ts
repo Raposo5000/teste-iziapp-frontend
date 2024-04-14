abbd613fd0510bfdd4f4165c9bd8baab564b50f9
raposo5000 <felipesantosdemenezes1998@gmail.com>
2024-04-14 18:31:41 -0300

    fix: env var

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.REACT_APP_API_URL || "http://localhost:5002"
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5002"
 
 const api = axios.create({
   baseURL: apiUrl,
